fed56f2766bad4a94367f4679b71bab030a702b8
dettyboy <dettyboy@icloud.com>
2023-12-10 09:39:05 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/bankcard_flip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bankcard_flip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+
+class FlipCard extends StatefulWidget {
+  const FlipCard({super.key});
+
+  @override
+  State<FlipCard> createState() => _FlipCardState();
+}
+
+class _FlipCardState extends State<FlipCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _frontRotation;
+  late Animation<double> _backRotation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _frontRotation = Tween<double>(begin: 0, end: 180).animate(_controller);
+    _backRotation = Tween<double>(begin: 180, end: 360).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+    _cardNumber.dispose();
+  }
+
+  void _toggleCard() {
+    if (_controller.isCompleted) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
+  }
+
+  final TextEditingController _cardNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flip Card Example'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: _toggleCard,
+                child: AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    return Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.002)
+                        ..rotateY(_controller.value < 0.5
+                            ? _frontRotation.value * 3.1415927 / 180
+                            : _backRotation.value * 3.1415927 / 180),
+                      alignment: Alignment.center,
+                      child: _controller.value < 0.5
+                          ? frontCard(_cardNumber.text, Colors.grey.shade500)
+                          : backCard(Colors.green),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.grey.shade300,
+                ),
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Card Number',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _cardNumber,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      'Card Holder',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Flexible(
+                            flex: 5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Expiry Date',
+                                  style: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ],
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Flexible(
+                            flex: 5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'CVV',
+                                  style: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ],
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      backgroundColor: Colors.grey.shade600,
+                      minimumSize: const Size(double.infinity, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Checkout',
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget frontCard(String _cardNumber, Color color) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: color,
+      ),
+      width: double.infinity,
+      height: 230,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Credit Card',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              FlutterLogo()
+            ],
+          ),
+          Text(_cardNumber)
+        ],
+      ),
+    );
+  }
+
+  Widget backCard(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: color,
+      ),
+      width: double.infinity,
+      height: 230,
+      child: const Center(
+        child: Text(
+          'dfghj',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
